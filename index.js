16431e09ec4a109cd084da2b8bca76fdf3dcfd00
Kaye Xie <xiekaye@gmail.com>
2022-10-13 20:44:05 -0400

    move  the partial html to to other div

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ inquirer
   </div>
       <ul class="list-group list-group-flush">
         <li class="list-group-item">Name: ${manager.name}</li>
-        <li class="list-group-item">Employee ID:${manager.employeeID} </li>
-        <li class="list-group-item">Email Address: ${manager.emailaddress}</li>
+        <li class="list-group-item">Employee ID:${manager.id} </li>
+        <li class="list-group-item">Email Address: ${manager.email}</li>
         <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
       </ul>
   </div>
@@ -83,8 +83,8 @@ function internInfo() {
    </div>
    <ul class="list-group list-group-flush">
      <li class="list-group-item">Name: ${intern.name}</li>
-     <li class="list-group-item">Employee ID: ${intern.employeeID}</li>
-     <li class="list-group-item">Email Address: ${intern.emailaddress}</li>
+     <li class="list-group-item">Employee ID: ${intern.id}</li>
+     <li class="list-group-item">Email Address: ${intern.email}</li>
      <li class="list-group-item">School: ${intern.school}</li>
    </ul>
    </div>
@@ -126,8 +126,8 @@ function internInfo() {
       </div>
       <ul class="list-group list-group-flush">
         <li class="list-group-item">Name: ${engineer.name}</li>
-        <li class="list-group-item">Employee ID: ${engineer.employeeID}</li>
-        <li class="list-group-item">Email Address: ${engineer.emailaddress}</li>
+        <li class="list-group-item">Employee ID: ${engineer.id}</li>
+        <li class="list-group-item">Email Address: ${engineer.email}</li>
         <li class="list-group-item">Github Username: <a href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a></li>
       </ul>
       </div>
@@ -183,12 +183,12 @@ function generateHtml(){
         </div>
       </div>
     <div id="employee">
-    </div>
     ${myteam}
+    </div>
 </body>
 </html>
   `
-  fs.writeFile('index.html', myPage, (err) => {
+  fs.writeFile('./dist/index.html', myPage, (err) => {
     err? console.log(err) :  console.log('Sucessfully generate the html file!')
   })
 }
